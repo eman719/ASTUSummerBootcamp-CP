891f0d64762cf84ccfd57f695817a20b6abca723
eman719 <emanabdullaziz719@gmail.com>
2026-06-12 05:07:27 +0300

    Time: 0 ms (100%), Space: 9 MB (31.22%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int totalScore = 0;
+        for (size_t i = 0; i < s.length() - 1; ++i) {
+            totalScore += std::abs(s[i] - s[i + 1]);
+        }
+        return totalScore;
+    
+    }
+};
